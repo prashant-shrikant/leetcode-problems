ff88bbc87a9a403558e332c9d0fa14d73d1425c0
prashant shrikant <prashantshrikant@mac.local>
2020-11-28 21:29:37 -0800

    improved solution using for of and removing regular expressions

--- easy/countCharacter.js
+++ easy/countCharacter.js
@@ -18,3 +18,27 @@ const countCharacters = (str) => {
 
 const result = countCharacters("Prashant Shrikant");
 console.log(result);
+
+// Improved without regular expressions
+const countCharactersImproved = str => { 
+  let obj = {};
+  for (let char of str) {
+    if (isAlphanumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  } return obj;
+}
+
+const isAlphanumeric = char => {
+  const code = char.charCodeAt(0);
+  if (!(code > 47 && code < 58 ) && // numeric 0-9
+      !(code > 64 && code < 91 ) && // upper alphabets A-Z
+      !(code > 96 && code < 123 )) { // lower alphabets a-z
+    return false;
+  }
+  return true; 
+}
+
+const resultImproved = countCharactersImproved("Prashant Shrikant");
+console.log(resultImproved);
