f3862aadcbbae0b8d839ce8f00054f93660278cf
prashant shrikant <prashantshrikant@mac.local>
2020-11-28 21:33:53 -0800

    Improved solution using for of

--- easy/countCharacter.js
+++ easy/countCharacter.js
@@ -1,6 +1,8 @@
 
 // Write a program to count the the total occurance of each character in a string
 
+// Input: "Prashant Shrikant"
+// Output:  p: 1, r: 2, a: 3, s: 2, h: 2, n: 2, t: 2, i: 1, k: 1 
 
 const countCharacters = (str) => {
   let obj = {};
@@ -18,3 +20,27 @@ const countCharacters = (str) => {
 
 const result = countCharacters("Prashant Shrikant");
 console.log(result);
+
+// Improved without regular expressions
+const countCharactersImproved = str => { 
+  let obj = {};
+  for (let char of str) {
+    if (isAlphanumeric(char)) {
+      char = char.toLowerCase();
+      obj[char] = ++obj[char] || 1;
+    }
+  } return obj;
+}
+
+const isAlphanumeric = char => {
+  const code = char.charCodeAt(0);
+  if (!(code > 47 && code < 58 ) && // numeric 0-9
+      !(code > 64 && code < 91 ) && // upper alphabets A-Z
+      !(code > 96 && code < 123 )) { // lower alphabets a-z
+    return false;
+  }
+  return true; 
+}
+
+const resultImproved = countCharactersImproved("Prashant Shrikant");
+console.log(resultImproved);
