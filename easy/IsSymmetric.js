d89b4833bbf37fbef5cdca3589a712b2a76c9efe
prashant shrikant <prashantshrikant@mac.local>
2021-01-04 18:55:24 -0800

    check if a tree is symmetric -- recursive soln

---
+++ easy/IsSymmetric.js
@@ -0,0 +1,11 @@
+const isSymmetric = root => {
+    return isMirror(root, root);
+};
+
+const isMirror = (t1, t2) => {
+    if (t1 === null && t2 === null) {return true;}
+    if (t1 === null || t2 === null) {return false;}
+    return (t1.val === t2.val) 
+            && isMirror(t1.right, t2.left) 
+            && isMirror(t1.left, t2.right)
+}
